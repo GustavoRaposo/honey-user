87030c70de4ded320e160cdeed035d25b6760fbd
Gustavo Raposo <gustavo_f.raposo@hotmail.com>
2025-08-06 14:29:28 -0300

    remove dist

--- dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AppService } from './app.service';
-import { UserService } from './modules/user/user.service';
-export declare class AppController {
-    private readonly appService;
-    private readonly userService;
-    constructor(appService: AppService, userService: UserService);
-    getHello(): string;
-}
--- dist/modules/user/dto/create-user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateUserDto {
-    name: string;
-    email: string;
-    password: string;
-    city?: string;
-    estate?: string;
-    country?: string;
-    birthDate?: string;
-}
